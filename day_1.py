53f8b085008df3843c11d61cac6e67db54edf466
Caroline-Wacera <carolinewacera4@gmail.com>
2026-04-09 15:24:40 +0300

    Learned data types and variables in Python

---
+++ day_1.py
@@ -0,0 +1,7 @@
+name = "Caroline"
+age = 33
+favourite_food = "Chicken"
+print(name)
+print(age)
+print(favourite_food)
+print(f"My name is {name}, I am {age} year and my favourite food is {favourite_food}.")
