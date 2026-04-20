008b83d2398ef9e06cf6460d000addfab33c387b
Caroline-Wacera <carolinewacera4@gmail.com>
2026-04-20 16:48:45 +0300

    Making corrections on the user input exercise

--- day_2.py
+++ day_2.py
@@ -11,24 +11,22 @@ print(f"Hello, {user_name}")
 Ask the user what they want to search
 Respond like a browser"""
 search_request = input("Enter your search request:")
-print(f"{search_request} AlX is a tech program for the youth in Africa")
-
+print(f"Showing results for: {search_request}")
 
 """ Exercise 3: Favorite Food App
  Task:
 Ask user their favorite food
 Respond with a sentence"""
 favourite_food = input("What is your favourite food?:")
-print(f"My favourite food is {favourite_food}.")
-
+print(f"Nice! {favourite_food} sounds delicious.")
 
 """Exercise 4: Simple Login (Important)
  Task:
 Ask for username and password
 Check if correct"""
-user_name = input("Enter the user name:")
-password = int(input("Enter the password:"))
-if user_name == "Caroline" and password == 1234:
+user_name = input("Enter username: ")
+password = input("Enter password: ")
+if user_name == "Caroline" and password == "1234":
     print("Access granted")
 else:
     print("Access denied")
@@ -38,8 +36,8 @@ else:
  Task:
 Ask user age
 Check if adult"""
-age = int(input("Enter your age:"))
-if age == 18:
+age = int(input("Enter your age: "))
+if age >= 18:
     print("User is an adult.")
 else:
     print("User is a child")
