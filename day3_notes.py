37e258f5a6394cf8376616aa0a54b9a05bb63721
Caroline-Wacera <carolinewacera4@gmail.com>
2026-04-23 22:12:27 +0300

    Day 3: Operators, logic and core backend concepts

---
+++ day3_notes.py
@@ -0,0 +1,22 @@
+# =========================
+# DAY 3 - OPERATORS & LOGIC
+# =========================
+
+# OPERATORS
+# +, -, *, /, %
+
+# COMPARISON OPERATORS
+# ==, !=, >, <, >=, <=
+
+# LOGICAL OPERATORS
+# and, or, not
+
+# TYPE CONVERSION
+# input() returns string
+# int() converts to number
+
+# API
+# A way systems communicate
+
+# DATABASE
+# Stores data
